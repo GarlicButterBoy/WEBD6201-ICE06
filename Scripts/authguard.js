57d8b780459b55c58fd18bac33ebc94234f56412
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-03-03 08:19:16 -0500

    first commit

---
+++ Scripts/authguard.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(() =>
+{
+//if the user isn't logged in, redirect to login.html
+if (!sessionStorage.getItem("user"))
+    {
+      //redirect to the secure area
+      location.href = "login.html";
+    }
+});
+
